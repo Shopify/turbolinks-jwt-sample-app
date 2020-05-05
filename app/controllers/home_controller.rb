16e5ae8b14b44c1917acb6153c4bd130428bca29
Mike Ragalie <mike.ragalie@shopify.com>
2020-05-05 09:26:54 -0400

    Generator output

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class HomeController < AuthenticatedController
+  def index
+    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    @webhooks = ShopifyAPI::Webhook.find(:all)
+  end
+end
