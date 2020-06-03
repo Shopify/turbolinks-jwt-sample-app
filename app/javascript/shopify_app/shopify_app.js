807096838b6ee2ea4bbda383830c7b07af581cdb
Maria Sitkovets <maria.sitkovets@shopify.com>
2020-06-03 16:38:14 -0400

    Retrieve session token using app-bridge-utils

--- app/javascript/shopify_app/shopify_app.js
+++ app/javascript/shopify_app/shopify_app.js
@@ -1,6 +1,8 @@
-document.addEventListener('DOMContentLoaded', async () => {
-  var data = document.getElementById('shopify-app-init').dataset;
-  var AppBridge = window['app-bridge'];
+import { getSessionToken } from "@shopify/app-bridge-utils";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  var data = document.getElementById("shopify-app-init").dataset;
+  var AppBridge = window["app-bridge"];
   var createApp = AppBridge.default;
   window.app = createApp({
     apiKey: data.apiKey,
@@ -15,7 +17,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   // Wait for the token before trying to load an authenticated page
   await retrieveToken(app);
-  Turbolinks.visit('/home');
+  Turbolinks.visit("/home");
 
   // Keep requesting the token every 50 seconds (I don't think we can wait for the token inline in request-start
   // event listener)
@@ -23,27 +25,22 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 async function retrieveToken(app) {
-  var AppBridge = window['app-bridge'];
-  app.dispatch(AppBridge.actions.SessionToken.request());
-
-  window.sessionToken = await new Promise((resolve) => {
-    app.subscribe(AppBridge.actions.SessionToken.ActionType.RESPOND, (data) => {
-      resolve(data.sessionToken || '');
-    });
-  });
+  window.sessionToken = await getSessionToken(app);
 }
 
 function keepRetrievingToken(app) {
-  setInterval(() => { retrieveToken(app) }, 50000);
+  setInterval(() => {
+    retrieveToken(app);
+  }, 50000);
 }
 
-document.addEventListener("turbolinks:request-start", function(event) {
+document.addEventListener("turbolinks:request-start", function (event) {
   var xhr = event.data.xhr;
   xhr.setRequestHeader("Authorization", "Bearer " + window.sessionToken);
 });
 
-document.addEventListener("turbolinks:render", function() {
-  $('form, a[data-method=delete]').on('ajax:beforeSend', function(event) {
+document.addEventListener("turbolinks:render", function () {
+  $("form, a[data-method=delete]").on("ajax:beforeSend", function (event) {
     const xhr = event.detail[0];
     xhr.setRequestHeader("Authorization", "Bearer " + window.sessionToken);
   });
