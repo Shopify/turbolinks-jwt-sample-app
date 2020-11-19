4493194fadb6b027f07718c0699f8c1096616de0
Nabeel Ahsen <nabeel.ahsen@shopify.com>
2020-11-19 14:40:55 -0500

    Create controller callback to authenticate a request

--- Gemfile
+++ Gemfile
@@ -51,4 +51,4 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-gem 'shopify_app', '~> 14.0.0'
+gem 'shopify_app', '~> 16.1.0'
--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class AuthenticatedController < ApplicationController
+  include ShopifyApp::EnsureAuthenticatedLinks
   include ShopifyApp::Authenticated
-
-  before_action :shop_origin
-
-  def shop_origin
-    @shop_origin = current_shopify_domain
-  end
 end
