1830ab0cab9b8907ded2df92a21693af9681a9e4
Travis Donia <travis.donia@shopify.com>
2021-07-14 12:14:24 -0400

    explicitly use AppBridge@2 and host instead of shopOrigin

--- app/controllers/splash_page_controller.rb
+++ app/controllers/splash_page_controller.rb
@@ -2,8 +2,4 @@ class SplashPageController < ApplicationController
   include ShopifyApp::EmbeddedApp
   include ShopifyApp::RequireKnownShop
   include ShopifyApp::ShopAccessScopesVerification
-
-  def index
-    @shop_origin = current_shopify_domain
-  end
 end
